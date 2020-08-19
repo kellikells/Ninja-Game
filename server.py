f77a009ba9671ab8c41b8f83ce4c1fb8e3af1021
kelli <kellikells@gmail.com>
2020-08-18 22:38:14 -0700

    server.py creates random values for each button, buttons work and display total gold and last activity

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, session
+
 import random   # to create random gold amounts
 app = Flask(__name__)
 
@@ -7,35 +8,72 @@ app.secret_key = 'ThisIsSecret'   # secret key required for session
 # =======================================
 @app.route('/')
 def index():
+
     if not 'totalGold' in session:
         session['totalGold'] = 0
 
     if not 'farm' in session:
-        session['farm'] = random.randrage(10,21)
+        session['farm'] = random.randrange(10, 21) 
 
     if not 'cave' in session:
-        session['cave'] = random.randrage(5,11)
+        session['cave'] = random.randrange(5, 11)
 
     if not 'house' in session:
-        session['house'] = random.randrage(2,6)
+        session['house'] = random.randrange(2, 6)
 
     if not 'casino' in session:
-        session['casino'] = random.randrage(-50,51)
-
-    print(session['totalGold'])
-    print(session['farm'])
-    print(session['cave'])
-    print(session['house'])
-    print(session['casino'])
+        session['casino'] = random.randrange(-50, 51)
     
+    if not 'activites' in session:
+        session['activites'] = ""
+
+    print("totalGold:", session['totalGold'])
+    print("farm", session['farm'])
+    print("cave", session['cave'])
+    print("house", session['house'])
+    print("casino", session['casino'])
 
     return render_template("index.html")
 
 
 
-
-# @app.route('/process_money', methods=['POST'])
+# =======================================
+# add to totalGold according to hidden input value (ex: farm)
+@app.route('/process_money', methods=['POST'])
 def process_money():
+    
+    print(request.form['building'])
+    
+    if request.form['building'] == "farm":
+
+        session['totalGold'] += session['farm']
+        
+        print("farm:", session['farm'])
+        print("new total gold:", session['totalGold'])
+
+        session['activities'] = ('Earned', session['farm'], 'gold from farm')   
+      
+
+        session['farm'] = random.randrange(10, 21) 
+
+    if request.form['building'] == "cave":
+        session['totalGold'] += session['cave']
+        session['activities'] = ('Earned', session['cave'], 'gold from cave') 
+        session['cave'] = random.randrange(5, 11)
+
+
+    if request.form['building'] == "house":
+        session['totalGold'] += session['house']
+        session['activities'] = ('Earned', session['house'], 'gold from house') 
+        session['house'] = random.randrange(2, 6)
+
+    if request.form['building'] == "casino":
+        session['totalGold'] += session['casino']
+        session['activities'] = ('Earned', session['casino'], 'gold from casino') 
+        session['casino'] = random.randrange(-50, 51)
+
+    return redirect('/')
+
     
 
 
